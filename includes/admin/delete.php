d363c6823f5738dacc76765c222fc355aa1121d6
srgg6701 <srgg67@gmail.com>
2014-05-07 01:50:47 +0400

    Исправлена ошибка в скрипте удаления записей.

--- includes/admin/delete.php
+++ includes/admin/delete.php
@@ -1,6 +1,6 @@
 <?php
 // если запись удалена, возвращаем её id
-if($connect->exec("DELETE FROM $table WHERE id = $segments[3]")){ // table id, record id
+if($connect->exec("DELETE FROM $segments[2] WHERE id = $segments[3]")){ // table id, record id
     echo $segments[3];
     exit();
 }
