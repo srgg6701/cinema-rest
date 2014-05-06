e700bcba604fd3f3f7078c0db033d194ff46b9f9
srgg6701 <srgg67@gmail.com>
2014-05-06 15:02:30 +0400

    Выставлена кодировка обработчика данных таблицы.

--- api/admin/post.php
+++ api/admin/post.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 if($_SERVER['REQUEST_METHOD']!='POST')
     die('Ты куда собрался, Малыш? Сюда можно ходить только методом POST!');
 $query = "INSERT INTO $_POST[table] (";
