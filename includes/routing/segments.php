56cd1f80db3f8f361475b9ea31233df143206230
srgg6701 <srgg67@gmail.com>
2014-05-08 11:23:06 +0400

    Модифицирован сегментатор.

--- includes/routing/segments.php
+++ includes/routing/segments.php
@@ -2,6 +2,9 @@
 $location = explode("/",$_SERVER['REQUEST_URI']);
 // распарсить URL на сементы подключения файлов
 $segments = array();
-foreach(range(1,count($location)-1) as $index) { // /site_name/segment1/segment2/segment3
-    isset($location[$index])? :$segments[$index]=$location[$index];
-}
+foreach(range(0,count($location)-1) as $index) { // /site_name/segment1/segment2/segment3
+    echo "<div>index = $index</div>";
+    if(isset($location[$index])&&$location[$index])
+        $segments[$index-1]=$location[$index];
+    else echo "<div>skipped index: $index</div>";
+}   var_dump("<pre>",$segments,"<pre/>");
