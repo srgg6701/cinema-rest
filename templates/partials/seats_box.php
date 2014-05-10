1699404b6d7689e333bb4a4d794dcaf4f31be05c
srgg6701 <srgg67@gmail.com>
2014-05-10 20:16:09 +0400

    Создан прототип обработчика заказа.

--- templates/partials/seats_box.php
+++ templates/partials/seats_box.php
@@ -1,4 +1,6 @@
-<?php header('Content-Type: text/html; charset=utf-8');
+<?php 
+session_start();
+header('Content-Type: text/html; charset=utf-8');
 require_once dirname(__FILE__)."/../../includes/routing/path.php"?>
 <form name="user-form" id="user-form" enctype="application/x-www-form-urlencoded" class="clearfix order" method="post" action="<?php
 echo SITE_ROOT;?>">
@@ -6,7 +8,10 @@ echo SITE_ROOT;?>">
         <h4>Укажите нужные места <div class="floatRight close" onclick="hideBox()">Закрыть окно</div></h4>
         <div id="seats" class="clearfix">
         </div>
-        <input type="hidden" id="seance-id" name="seance-id" value="">
+        <input type="hidden" id="active-user-id" name="active_user_id" value="<?php
+			echo $_SESSION['active_user_id'];
+        ?>">
+        <input type="hidden" id="seance-id" name="seance_id" value="">
         <button name="user_post_data" value="includes/user/post_order" type="submit" id="btn-make-order">Оформить заказ!</button>
     </div>
 </form>
